15512b0409d1015e727d0950e5db9d127bce0ba1
gregcorbett <greg.corbman@gmail.com>
2017-08-02 21:40:06 +0100

    Add first minimax implementation

--- src/CPU.py
+++ src/CPU.py
@@ -1,6 +1,6 @@
 """This file contains the CPU (player) class."""
 
-import random
+import copy
 
 from src.Player import Player
 
@@ -13,9 +13,60 @@ class CPU(Player):
 
     def move(self, board):
         """Make a move."""
-        moved = False
-        while not moved:
-            x_cord = random.randint(0, board.width - 1)
-            y_cord = random.randint(0, board.height - 1)
+        print('Player %s is thinking!: ' % self.marker)
 
-            moved = self._place_marker((x_cord, y_cord), board)
+        (x_cord, y_cord) = self.minimax(board)
+
+        self._place_marker((x_cord, y_cord), board)
+
+    def minimax(self, board):
+        """Use the minimax algorthim to determine the best move to make."""
+        moves = board.get_possible_moves()
+        best_move = moves[0]
+        best_score = float('-inf')
+        for move in moves:
+            clone_state = copy.deepcopy(board)
+            self._place_marker(move, clone_state, 'O')
+            score = self.min_play(clone_state)
+            if score > best_score:
+                best_move = move
+                best_score = score
+
+        return best_move
+
+    def max_play(self, board):
+        """Helper method for minimax."""
+        if board.is_there_a_winner() == 'X':
+            # then X has won
+            return -100
+        elif board.get_possible_moves() == []:
+            # then it is a draw
+            return 0
+
+        best_score = float('-inf')
+        for move in board.get_possible_moves():
+            clone_state = copy.deepcopy(board)
+            self._place_marker(move, clone_state, 'O')
+            score = self.min_play(clone_state)
+            if score > best_score:
+                best_score = score
+
+        return best_score
+
+    def min_play(self, board):
+        """Helper method for minimax."""
+        if board.is_there_a_winner() == 'O':
+            # then O has won
+            return 100
+        elif board.get_possible_moves() == []:
+            return 0
+
+        best_score = float('inf')
+        for move in board.get_possible_moves():
+            clone_state = copy.deepcopy(board)
+            self._place_marker(move, clone_state, 'X')
+            score = self.max_play(clone_state)
+            if score < best_score:
+                best_score = score
+
+        return best_score
